849f792015c810ea62db02667b87eb7a2c92366c
segatelepnyak@gmail.com <segatelepnyak@gmail.com>
2024-11-05 13:36:53 +0300

    First

---
+++ bot.py
@@ -0,0 +1,92 @@
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, CommandHandler
+
+from gpt import ChatGptService
+from util import *
+
+
+async def start(update, context):
+    dialog.mode = "main"
+    text = load_message("main")
+    await send_photo(update, context, "main")
+    await send_text(update, context, text)
+
+    await show_main_menu(update, context, {
+        "start": "главное меню бота",
+        "questions": "генерация вопросов с вариантами ответов 🧠",
+        "results": "результаты тестирования ✅",
+        "gpt": "задать вопрос чату GPT "
+    })
+
+
+async def gpt(update, context):
+    dialog.mode = "gpt"
+    text = load_message("gpt")
+    await send_photo(update, context, "gpt")
+    await send_text(update, context, text)
+
+
+async def gpt_dialog(update, context):
+    text = update.message.text
+    prompt = load_prompt("gpt")
+    my_message = await send_text(update, context, "ChatGPT 🧠 занимается генерацией вашего профиля. "
+                                                      "Подождите пару секунд...")
+    answer = await chatgpt.send_question(prompt, text)
+    await my_message.edit_text(answer)
+
+
+
+async def questions(update, context):
+    dialog.mode = "questions"
+    text = load_message("questions")
+    await send_text(update, context, text)
+
+
+async def questions_dialog(update, context):
+    text = update.message.text
+    prompt = load_prompt("questions")
+    my_message = await send_text(update, context, "ChatGPT 🧠 занимается генерацией вашего профиля. "
+                                                  "Подождите пару секунд...")
+    answer = await chatgpt.send_question(prompt, text)
+    await my_message.edit_text(answer)
+
+
+async def results(update, context):
+    dialog.mode = "results"
+    text = load_message("results")
+    await send_text(update, context, text)
+
+
+async def results_dialog(update, context):
+    text = update.message.text
+    prompt = load_prompt("results")
+    my_message = await send_text(update, context, "ChatGPT 🧠 занимается генерацией вашего профиля. "
+                                                  "Подождите пару секунд...")
+    answer = await chatgpt.send_question(prompt, text)
+    await my_message.edit_text(answer)
+
+
+async def hello(update, context):
+    if dialog.mode == "gpt":
+        await gpt_dialog(update, context)
+    if dialog.mode == "questions":
+        await questions_dialog(update, context)
+    if dialog.mode == "results":
+        await results_dialog(update, context)
+    else:
+        await send_text(update, context, "Выберите необходимые для вас пункты.")
+
+dialog = Dialog()
+dialog.mode = None
+
+
+chatgpt = ChatGptService(token="*") # Put OpenAI token
+
+app = ApplicationBuilder().token("*").build() # Put Telegram token
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler("questions", questions))
+app.add_handler(CommandHandler("results", results))
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+
+app.run_polling()
